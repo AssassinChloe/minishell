d8fe1766b47145f33df83036268402d31daa24a3
vomercier <81368227+vomercier@users.noreply.github.com>
2022-01-21 12:14:26 +0100

    ajout lexer simple

--- minishell.h
+++ minishell.h
@@ -1,13 +1,22 @@
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
 
+# define T_ERR 1
+# define T_PIPE 2
+# define T_CMD 3
+# define T_FLAG 4
+# define T_LOWER 5
+# define T_GREATER 6
+# define T_LLOWER 7
+# define T_GGREATER 8
+# define T_STRING 9
 
 
 void    ft_parse(char *str);
@@ -17,5 +26,7 @@ int ft_issimplequote(char c);
 int ft_isdoublequote(char c);
 int ft_ispipe(char c);
 
+int	ft_strcmp(char *s1, char *s2);
+int get_token_type(char *str);
 
 #endif
