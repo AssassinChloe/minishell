641c44297c050bfb5449d62d3e2cbbf215f82d89
Chloe ASSASSIN <cassassi@e2r12p13.clusters.42paris.fr>
2022-02-07 17:32:19 +0100

    ca crash moins mais pb de fd

--- pipe_test.c
+++ pipe_test.c
@@ -2,14 +2,13 @@
 
 void    ft_pipe(t_list *commandlist)
 {
-    int id;
+    int pid;
     t_cmd *command;
     t_redir *tuyau;
-    int i;
+    int		status = 0;
 
-    i = 0;
     command = (t_cmd *)commandlist->content;
-    tuyau = malloc(sizeof(t_redir));
+
     if (g_data.nb_pipe == 0)
     {
         ft_divide_redirection(commandlist);
@@ -17,39 +16,41 @@ void    ft_pipe(t_list *commandlist)
     }
     else 
     {
+        tuyau = malloc(sizeof(t_redir));
         if (pipe(command->pip) < 0)
             printf("error pipe\n");
-        id = fork();
-        if (id == 0)
+        pid = fork();
+        if (pid == 0)
         {
             if (close(command->pip[0]) == -1)
                 printf ("error close\n");
-            tuyau->fd = command->pip[0];
-            commandlist = commandlist->next;
-            command = (t_cmd *)commandlist->content;
-            i++;
-            ft_divide_redirection(commandlist);
+            tuyau->fd = command->pip[1];
+
+        /*    ft_divide_redirection(commandlist);
             if (command->redir_nb > 0)
                 ft_redirstd(tuyau, command->redir->fd);
-            else
+            else*/
                 ft_redirstd(tuyau, STDOUT_FILENO);
             ft_execution_test(command);
             if (dup2(tuyau->fdsave, STDOUT_FILENO) == -1)
-                printf("error reestablish stdout\n");
-            if (command->redir_nb > 0)
-                ft_endredir(command);
+                printf("error reestablish stdin\n");
+          /*  if (command->redir_nb > 0)
+                ft_endredir(command);*/
             if (close(command->pip[1]) == -1)
                 printf ("error close\n");
         }
         else 
         {
+            waitpid(pid, &status, 0);
             if (close(command->pip[1]) == -1)
                 printf ("error close\n");
-            tuyau->fd = command->pip[1];
-            ft_divide_redirection(commandlist);
+            tuyau->fd = command->pip[0];
+            commandlist = commandlist->next;
+            command = (t_cmd *)commandlist->content;
+            /*ft_divide_redirection(commandlist);
             if (command->redir_nb > 0)
                 ft_redirstd(tuyau, command->redir->fd);
-            else
+            else*/
                 ft_redirstd(tuyau, STDIN_FILENO);
             ft_execution_test(command);
             if (dup2(tuyau->fdsave, STDIN_FILENO) == -1)
@@ -58,6 +59,7 @@ void    ft_pipe(t_list *commandlist)
                 ft_endredir(command);
             if (close(command->pip[0]) == -1)
                 printf ("error close\n");
+		    kill(pid, SIGTERM);
         }
         free(tuyau);
         tuyau = NULL;
