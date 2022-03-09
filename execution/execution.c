2f555034ac6c0ae8f61f207effb402fe8287e75b
Chloe ASSASSIN <cassassi@e2r9p9.clusters.42paris.fr>
2022-03-09 16:21:30 +0100

    plop

--- execution/execution.c
+++ execution/execution.c
@@ -16,6 +16,7 @@ void	exec_cmd(t_cmd *cmd)
 {
 	if (is_valid_cmd(cmd) == 0)
 	{
+		printf("%s\n", cmd->av[0]);
 		if (execve(cmd->av[0], cmd->av, /**(g_data.env_p)*/NULL) < 0)
 				perror("minishell");
 	}
